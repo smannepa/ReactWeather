f479f2661a966d241c2a2bd176036321f0f1c05c
Susheel Mannepalli <susheel.mannepalli@gmail.com>
2016-12-22 21:29:35 -0800

    Styled Main page to include a grid and provided example links on examples.jsx

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,27 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to the Example page!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>A few example locations to try out.</p>
+        <ol>
+          <li>
+            <Link to='/?location=Redmond, WA'>Redmond, WA</Link>
+          </li>
+          <li>
+            <Link to='/?location=Rio, Brazil'>Rio, Brazil</Link>
+          </li>
+        </ol>
       </div>
     );
   };
 
 module.exports = Examples;
+
+var Examples = React.createClass({
+  render: function () {
+
+  }
+});
